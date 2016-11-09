369c14f7c8102c5e0913201bc335d6c63b5ee5cd
ynori7 <ynori7@yahoo.com>
2016-11-09 19:12:42 +0100

    Now using the new go-irc library where irc-connection logic has been extracted to

--- ircbot.go
+++ ircbot.go
@@ -1,68 +1,67 @@
 package main
 
 import (
-	"bufio"
 	"io/ioutil"
-	"fmt"
 	"os"
 	"log"
 	"strings"
 	"errors"
 
 	"github.com/ynori7/ircbot/ircconfig"
-	"github.com/ynori7/ircbot/ircutil"
+	"github.com/ynori7/go-irc/client"
+	"github.com/ynori7/go-irc/model"
 	"github.com/ynori7/ircbot/library"
 )
+
+var config ircconfig.IrcConfig
 
 /**
  * Performs the designated action according to the content of the message received.
  */
-func HandleMessage(conn ircutil.IrcConnection, message string) {
-	line := conn.ParseLine(message)
-
-	if line.Type == "PING" {
-		conn.Pong(line.Message)
+func HandleMessage(conn client.Client, message model.Message) {
+	if message.Type == "PING" {
+		conn.Pong(message.Message)
 	}
 
-	if line.Type == "001" { //001 appears when we've connected and the server starts talking to us
-		for _, ch := range conn.Config.Channels { //join all the channels in the config
+	if message.Type == "001" { //001 appears when we've connected and the server starts talking to us
+		for _, ch := range config.Channels { //join all the channels in the config
 			conn.JoinChannel(ch)
 		}
 	}
 
-	if line.Type == "KICK" && line.Message == conn.Config.Nick {
-		conn.JoinChannel(line.Location) //rejoin the channel I was kicked from
+	if message.Type == "KICK" && message.Message == conn.Nick {
+		conn.JoinChannel(message.Location) //rejoin the channel I was kicked from
 	}
 
-	if line.Type == "JOIN" && line.Sender.Nick != conn.Config.Nick { //Greet user who joined channel
-		conn.SendMessage(conn.Config.GetRandomGreeting() + " " + line.Sender.Nick, line.Location)
+	if message.Type == "JOIN" && message.Sender.Nick != conn.Nick { //Greet user who joined channel
+		conn.SendMessage(config.GetRandomGreeting() + " " + message.Sender.Nick, message.Location)
 	}
-	if line.Type == "PRIVMSG" {
-		Conversation(conn, line)
+	if message.Type == "PRIVMSG" {
+		Conversation(conn, message)
 	}
 }
 
 /**
  * Handles conversational type messages like talking to other users.
  */
-func Conversation(conn ircutil.IrcConnection, line ircutil.IrcMessage) {
-	location := line.Location
+func Conversation(conn client.Client, message model.Message) {
+	location := message.Location
 	//Handle the case when user is talking to me in private message, not in channel
-	if(line.Location == conn.Config.Nick) {
-		location = line.Sender.Nick
+	if(message.Location == conn.Nick) {
+		location = message.Sender.Nick
 	}
 
-	if strings.Contains(line.Message, conn.Config.Nick) { //someone is talking to me or about me
-		words := strings.Fields(line.Message)
+	if strings.Contains(message.Message, conn.Nick) { //someone is talking to me or about me
+		words := strings.Fields(message.Message)
 
 		//respond to greetings
-		if in_array(conn.Config.Greetings, strings.ToLower(words[0])){
-			conn.SendMessage(conn.Config.GetRandomGreeting(), location)
+		if in_array(config.Greetings, strings.ToLower(words[0])){
+			conn.SendMessage(config.GetRandomGreeting(), location)
 		}
 	}
 
-	if strings.Contains(line.Message, "github.com") {
-		githubResponse := library.HandleGithubLink(line.Message)
+	if strings.Contains(message.Message, "github.com") {
+		githubResponse := library.HandleGithubLink(message.Message)
 		if githubResponse != "" {
 			conn.SendMessage(githubResponse, location)
 		}
@@ -93,30 +92,17 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var config ircconfig.IrcConfig
+
 	if err := config.Parse(data); err != nil {
 		log.Fatal(err)
 	}
 
 	//Connect
-	conn := ircutil.IrcConnection{Config: config}
-	err = conn.Connect()
-
+	conn, err := client.NewConnection(config.ConnectionString, config.UseSSL, config.Nick)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//Start reading from the connection
-	connbuf := bufio.NewReader(conn.Connection)
-	for{
-		str, err := connbuf.ReadString('\n')
-		if len(str)>0 {
-			fmt.Println(str)
-			go HandleMessage(conn, str) //handle message asynchronously so we can go back to listening
-		}
-		if err!= nil {
-			log.Fatal(err)
-		}
-	}
+	conn.Listen(HandleMessage)
 
 }
